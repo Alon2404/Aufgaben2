b13b1a6a0bb2131417f98fd410c396d66eebd81d
Alon Raci <alonraci8@gmail.com>
2021-11-30 15:05:55 +0100

    Datenstruckturen

---
+++ TicTacToe.java
@@ -0,0 +1,23 @@
+public class TicTacToe {
+}
+
+
+
+
+
+
+
+
+
+/* Idee
+
+Als erstes soll ein 3x3 Großes Feld ausgegeben werden.
+Der Spieler 1 wird gefragt welches Feld er beanspruchen möchte.
+Das neue Feld wird ausgegeben mit dem Beanspruchten Feld und so geht das Spiel geht dann so weiter
+das Programm prüft vor jeder nächsten eingabe ob einer schon gewonnen hat (3 in einer reihe)
+falls das der fall ist hat der Spieler gewonnen und das spiel geh zu ende (Mit einer For-loop oder ***while anforderung***.
+checken ob ein spieler gewonnen hat. "Keine if Anweisung für jede einzelen gewinn möglichkeit zu viele if´s".
+2D-Array Abfragen ob schon 3 in einer Reihe sind. für die 2 schräg gewinn möglichkeiten extra if´s.
+
+
+ */
